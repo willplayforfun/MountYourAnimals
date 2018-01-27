6c069ad98e5ae924fa51b30161350a718c8ec5f9
tpostley <tpostley@gmail.com>
2018-01-27 14:33:35 -0800

    Signal Functionality
    some shit

--- MountYourAnimals/Assets/Phone.cs
+++ MountYourAnimals/Assets/Phone.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class Phone : MonoBehaviour {
 
+	int signalBar;
+
+	public Sprite[] signalSprites;
+	public GameObject signalIndicator;
+	SpriteRenderer signalRenderer;
+
 	// Use this for initialization
 	void Start () {
 		
+		signalRenderer = signalIndicator.GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		signalIndicator.transform.position = transform.position + new Vector3(0,1,0);
 		
+	}	
+
+	public void AddSignal()
+	{
+		signalBar++;
+		signalRenderer.sprite = signalSprites[signalBar];
+		print(signalBar);
+	}
+	public void SubtractSignal()
+	{
+		signalBar--;
+		signalRenderer.sprite = signalSprites[signalBar];
+		print(signalBar);
 	}
 }
---
+++ MountYourAnimals/Assets/Signal.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signal : MonoBehaviour {
+
+	//int signalBar;
+
+	public GameObject phone;
+	Phone phoneScript;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		phoneScript = phone.GetComponent<Phone>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/// <summary>
+	/// Sent when another object enters a trigger collider attached to this
+	/// object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		print("TriggerEnter");
+		if(other.tag == "Phone")
+		{
+			phoneScript.AddSignal();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		print("TriggerExit");
+		if(other.tag == "Phone")
+		{
+			phoneScript.SubtractSignal();
+		}
+	}
+}
