923400fd8ac6e13051e927e92fb03a82c515c0f0
David Haynes <denglishdesign@gmail.com>
2018-01-27 20:27:09 -0800

    Replaced all art models and added giraffe

---
+++ MountYourAnimals/Assets/Giraffe.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Giraffe : Animal
+{
+    protected override void DoAbility()
+    {
+        base.DoAbility();
+    }
+}
